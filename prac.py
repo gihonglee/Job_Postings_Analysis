4269220eb36d0be6f5c1cb09dcc3a84cd1543d7a
Gihong Lee <gihonglee20@gmail.com>
2022-02-15 15:58:24 -0500

    Model Building

--- prac.py
+++
@@ -1,9 +0,0 @@
-from sklearn import datasets, linear_model
-from sklearn.model_selection import cross_val_score
-diabetes = datasets.load_diabetes()
-# X = diabetes.data[:150]
-# y = diabetes.target[:150]
-# lasso = linear_model.Lasso()
-# print(cross_val_score(lasso, X, y, cv=3))  
-
-print(diabetes.data)
